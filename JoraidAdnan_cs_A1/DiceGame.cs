698fe58a9a5a63f211267c381c5f2177083ea68f
Adnan Joraid <joraid@shernet.sheridancollege.ca>
2020-02-07 00:50:04 -0500

    Adding the game logic

--- JoraidAdnan_cs_A1/DiceGame.cs
+++ JoraidAdnan_cs_A1/DiceGame.cs
@@ -6,7 +6,161 @@ using System.Threading.Tasks;
 
 namespace JoraidAdnan_cs_A1
 {
-    public class DiceGame
+    class DiceGame
     {
+        public int PlayerOneScore;
+        public int PlayerTwoScore;
+        public int TurnPoints;
+        public int Counter;
+        public bool IsJackpot;
+        public int PlayerOneJackpotCount;
+        public int PlayerTwoJackpotCount;
+        List<int> PlayerOneJack = new List<int>();
+        List<int> PlayerTwoJack = new List<int>();
+
+
+        public List<int> Roll()
+        {
+
+            Random random = new Random();
+            List<int> DiceValues = new List<int>();
+            for (int RandomDice = 0; RandomDice < 2; RandomDice++)
+                DiceValues.Add(random.Next(1, 7));
+
+            GameCalculations(DiceValues);
+            return DiceValues;
+        }
+
+        public bool IsTie()
+        {
+            return PlayerOneScore == PlayerTwoScore;
+        }
+
+        public bool IsWinner()
+        {
+            return PlayerOneScore > PlayerTwoScore;
+        }
+
+        public void GameCalculations(List<int> DiceValues)
+        {
+
+            if (DiceValues[0] == DiceValues[1] && DiceValues[0] == 6)
+            {
+
+                if (Counter % 2 == 0)
+                {
+
+                    PlayerOneJack.Add(1);
+
+                    if (IsPlayerOneJackpot())
+                    {
+                        PlayerOneJackpotCount += 1;
+                        TurnPoints = 200;
+                        PlayerOneScore += TurnPoints;
+                    }
+                    else
+                    {
+                        TurnPoints = 100;
+                        PlayerOneScore += TurnPoints;
+                    }
+
+                }
+                else
+                {
+
+                    PlayerTwoJack.Add(1);
+
+                    if (IsPlayerTwoJackpot())
+                    {
+                        PlayerTwoJackpotCount += 1;
+                        TurnPoints = 200;
+                        PlayerTwoScore += TurnPoints;
+                    }
+                    else
+                    {
+                        TurnPoints = 100;
+                        PlayerTwoScore += TurnPoints;
+                    }
+
+                }
+            }
+
+            else if (DiceValues[0] == DiceValues[1] && DiceValues[0] != 6)
+            {
+                if (Counter % 2 == 0)
+                {
+                    PlayerOneJack.Add(0);
+                    TurnPoints = DiceValues[0] * 10;
+                    PlayerOneScore += TurnPoints;
+                }
+                else
+                {
+                    PlayerTwoJack.Add(0);
+                    TurnPoints = DiceValues[0] * 10;
+                    PlayerTwoScore += TurnPoints;
+                }
+
+            }
+            else
+            {
+                TurnPoints = 0;
+            }
+
+        }
+
+        public bool IsPlayerOneJackpot()
+        {
+            if (!PlayerOneGameOver())
+            {
+                if (PlayerOneJack.Count > 1)
+                {
+
+                    return PlayerOneJack[PlayerOneJack.Count - 1] == PlayerOneJack[PlayerOneJack.Count - 2] && PlayerOneJack[PlayerOneJack.Count - 1] == 1;
+
+                }
+
+            }
+            return false;
+
+        }
+
+        public bool IsPlayerTwoJackpot()
+        {
+            if (!PlayerTwoGameOver())
+            {
+                if (PlayerTwoJack.Count > 1)
+                {
+
+                    return PlayerTwoJack[PlayerTwoJack.Count - 1] == PlayerTwoJack[PlayerTwoJack.Count - 2] && PlayerTwoJack[PlayerTwoJack.Count - 1] == 1;
+
+                }
+
+            }
+            return false;
+        }
+
+
+
+        public bool PlayerOneGameOver()
+        {
+            if (PlayerOneJack.Count > 2)
+            {
+                return PlayerOneJack[PlayerOneJack.Count - 1] == PlayerOneJack[PlayerOneJack.Count - 2] && PlayerOneJack[PlayerOneJack.Count - 2] == PlayerOneJack[PlayerOneJack.Count - 3] && PlayerOneJack[PlayerOneJack.Count - 1] == 1;
+            }
+            return false;
+        }
+
+
+
+        public bool PlayerTwoGameOver()
+        {
+            if (PlayerTwoJack.Count > 2)
+            {
+                return PlayerTwoJack[PlayerTwoJack.Count - 1] == PlayerTwoJack[PlayerTwoJack.Count - 2] && PlayerTwoJack[PlayerTwoJack.Count - 2] == PlayerTwoJack[PlayerTwoJack.Count - 3] && PlayerTwoJack[PlayerTwoJack.Count - 1] == 1;
+            }
+            return false;
+        }
+
+
     }
 }
--- JoraidAdnan_cs_A1/MainPage.xaml.cs
+++ JoraidAdnan_cs_A1/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +23,130 @@ namespace JoraidAdnan_cs_A1
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private DiceGame _diceGame = new DiceGame();
+
+        int Count;
         public MainPage()
         {
             this.InitializeComponent();
+            RollButton.Content = "Roll (Player 1)";
+            P1Score.Text = "Player One Score: 0";
+            P2Score.Text = "Player Two Score: 0";
+            TurnScore.Text = "Turn Score: 0";
+            P1Jackpot.Text = $"Player 1 consecutive Jackpots count: 0";
+            P2Jackpot.Text = $"Player 2 consecutive Jackpots count: 0";
+            RollsLeft.Text = $"Rolls left: {(10 - Count)}";
+
         }
+
+        private void RollButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<int> DiceValues = _diceGame.Roll();
+
+
+            Count++; // counts how many times the user clicked the button roll 
+            _diceGame.Counter = Count;
+            if (Count != 10)
+            {
+
+                if (Count % 2 == 0)
+                {
+                    RollButton.Content = "Roll (Player 1)";
+                    TurnScore.Text = $"Turn Score: {_diceGame.TurnPoints}";
+                    P1Score.Text = $"Player One Score: {_diceGame.PlayerOneScore}";
+                    P1Jackpot.Text = $"Player 1 consecutive Jackpots count: {_diceGame.PlayerOneJackpotCount}";
+                    LeftDice.Text = DiceValues[0].ToString();
+                    RightDice.Text = DiceValues[1].ToString();
+                    RollsLeft.Text = $"Rolls left: {(10 - Count)}";
+
+                    if (_diceGame.IsPlayerOneJackpot())
+                    {
+                        MessageDialog GameOverMessage = new MessageDialog("Player One Got two consecutive jackpots!!!"); //game over message
+                        GameOverMessage.ShowAsync(); // display the message ie. input
+
+
+                    }
+                    else if (_diceGame.PlayerOneGameOver())
+                    {
+                        MessageDialog GameOverMessage = new MessageDialog("Game over. Player One Got three consecutive jackpots!!!"); //game over message
+                        GameOverMessage.ShowAsync(); // display the message ie. input
+                        EndGame();
+
+                    }
+
+                }
+
+                else if (Count % 2 == 1)
+                {
+                    RollButton.Content = "Roll (Player 2)";
+                    TurnScore.Text = $"Turn Score: {_diceGame.TurnPoints}";
+                    P2Score.Text = $"Player Two Score: {_diceGame.PlayerTwoScore}";
+                    P2Jackpot.Text = $"Player 2 consecutive Jackpots count: {_diceGame.PlayerTwoJackpotCount}";
+                    LeftDice.Text = DiceValues[0].ToString();
+                    RightDice.Text = DiceValues[1].ToString();
+                    RollsLeft.Text = $"Rolls left: {(10 - Count)}";
+
+                    if (_diceGame.IsPlayerTwoJackpot())
+                    {
+                        MessageDialog GameOverMessage = new MessageDialog("Player Two Got two consecutive jackpots!!!"); //game over message
+                        GameOverMessage.ShowAsync(); // display the message ie. input
+
+
+                    }
+                    else if (_diceGame.PlayerTwoGameOver())
+                    {
+                        MessageDialog GameOverMessage = new MessageDialog("Game over. Player Two Got three consecutive jackpots!!!"); //game over message
+                        GameOverMessage.ShowAsync(); // display the message ie. input
+                        _diceGame = new DiceGame();
+                        EndGame();
+
+                    }
+
+                }
+
+            }
+            if (Count == 10)
+            {
+                EndGame();
+
+            }
+
+
+
+
+        }
+
+        public void EndGame()
+        {
+            if (_diceGame.IsWinner())
+            {
+                MessageDialog GameOverMessage = new MessageDialog($"Game Over. The Winner is Player One with the overall score {_diceGame.PlayerOneScore}");
+                GameOverMessage.ShowAsync();
+            }
+            else if (!_diceGame.IsWinner() && !_diceGame.IsTie())
+            {
+                MessageDialog GameOverMessage = new MessageDialog($"Game Over. The Winner is Player Two with the overall score {_diceGame.PlayerTwoScore}");
+                GameOverMessage.ShowAsync();
+            }
+            else if (_diceGame.IsTie())
+            {
+                MessageDialog GameOverMessage = new MessageDialog($"Game Over. No winner 'TIE' Player one score is: {_diceGame.PlayerOneScore}, player two score is: {_diceGame.PlayerTwoScore}");
+                GameOverMessage.ShowAsync();
+            }
+
+            _diceGame = new DiceGame();
+            Count = 0; // reset the counter to 0, so the user can restart the game. 
+
+            LeftDice.Text = "1";
+            RightDice.Text = "1";
+            P1Score.Text = "Player One Score: 0";
+            P2Score.Text = "Player Two Score: 0";
+            RollButton.Content = "Roll (Player 1)";
+            TurnScore.Text = "Turn Score: 0";
+            P1Jackpot.Text = $"Player 1 consecutive Jackpots count: 0";
+            P2Jackpot.Text = $"Player 2 consecutive Jackpots count: 0";
+            RollsLeft.Text = $"Rolls left: {(10 - Count)}";
+        }
+    
     }
 }
